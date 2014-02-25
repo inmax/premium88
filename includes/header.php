75c17065fb236c6a6cc95bde27441cfca76b9779
inmax <inmaculadaminarroruiz@gmail.com>
2014-02-25 10:25:14 +0100

    sdf

--- includes/header.php
+++ includes/header.php
@@ -11,25 +11,27 @@
 			<a href="index.php" id="logo" title="title">
 				<img src="images/premium88.png"/>
 			</a>
-			<h2>
-				SPAIN´S FINEST FOODS & DRINKS
-			</h2>
-			<nav>
-				<ul>
-					<li >
-						<a href="about.php">About us</a>	
-					</li>
-					<li>
-						<a href="#"><span class="icon_f"></span>787df5f5d
-						</a>
-					</li>
-					<li>
-						<a class="right" href="contact.php">Contac us </a>
-							
-					</li>
-					
-				</ul>
-			</nav>
+			<div>
+				<h2>
+					SPAIN´S FINEST FOODS & DRINKS
+				</h2>
+				<nav>
+					<ul>
+						<li >
+							<a href="about.php">About us</a>	
+						</li>
+						<li>
+							<a href="#"><span class="icon_f"></span>787df5f5d
+							</a>
+						</li>
+						<li>
+							<a class="right" href="contact.php">Contac us </a>
+								
+						</li>
+						
+					</ul>
+				</nav>
+			<div>
 		</div>
 	</div><!-- rawpper header-->
 </header>
