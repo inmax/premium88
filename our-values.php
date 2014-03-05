c7c5df89d7c71dfaed0491d0625aa294e1e7273b
Bartoloxs <crls.barronn@gmail.com>
2014-03-05 20:12:43 +0100

    subo cambios

--- our-values.php
+++ our-values.php
@@ -14,26 +14,26 @@
     <div id="wrap">
         <?php include("includes/header.php");?>
         <?php include("includes/header_responsive.php");?>
-        <div id="container_contact">
-            <div id="contact" class="cuerpo_a">
-                <h3 class="titulos-section P30 PBot">Contact us</h3>
-                <p  class="titulos-b P15 PBot">Get In Touch</p>
-                <p class="P30 PBot">Fill out the form below or call us at 917.670.3314.</p>
-            
-                <form action = "controllers/mail.php" method="POST" enctype="application/x-www-form-urlencoded">
-                        <label for="name">Name</label> 
-                        <input type="text" name = "name" id="name" required/>
-                       <!-- <input  type="tel" name="telefono" maxlength="9" placeholder="Teléfono"/>-->
-                        <label for="email">Email</label> 
-                        <input type="email " name="email" id="email" required/>
-                        <label for="message">Message</label> 
-                        <textarea name="message" id="message"maxlength = "200" required> 
-                        </textarea>
-                        <input id="submit" type="submit" value="Submit" class="btn"/>
-                </form>
+        <div id="container_values">
+            <div class="values cuerpo_b">
+                <h3 class="titulos-section P40 PBot">Our Values</h3>
+                <h4 class="P15 PBot titulos-b">Quality: </h4>
+                <p class="P30 PBot">
+                We aim to provide the best gourmet foods available in Spain. Quality is our number one priority and we want to be known for it. The products we offer have been carefully chosen to please the most refined tastes.
+                </p>
+                <h4 class="P15 PBot titulos-b">Reliability:</h4>
+                <p class="P30 PBot">
+                   We want to be absolutely reliable in all the transactions we make, so we will do them through verified bank payment card, that way both parts will be absolutely sure that they will close the deal with 100% of success.
+                </p>
+                <h4 class="P15 PBot titulos-b">
+                    Health care:
+                </h4>
+                <p class="P30 PBot">
+                    We try to choose the best products from the Mediterranean diet, one of the world´s healthiest. We have excellent wines, olive oil, tuna, anchovies... All known for their nutritional values. 
+                </p>
             </div>
             <div>
-                <img src="images/premium-contact.jpg" height="590" width="412"/>
+                <img src="images/our-values.jpg" class="values_img"/>
             </div>    
         </div><!--div container-contact -->
     <?php include("includes/footer.php");?>
